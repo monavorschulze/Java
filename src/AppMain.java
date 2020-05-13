b6a5648537cc81fe6008f6fc80b7a2ef5531fd5e
Mona Vorschulze <>
2020-05-13 11:42:28 +0200

    new function boolean

--- src/AppMain.java
+++ src/AppMain.java
@@ -1,5 +1,30 @@
 public class AppMain {
     public static void main(String[] args){
-        System.out.println("Hallo Leute!");
+        String name =" Dude ";
+
+        boolean sweet = true;
+
+        System.out.println("Hallo Leute!" + name + "Sweet "+ sweet);
+    sweet = false;
+
+    int firstInt = 100_000_000;
+    int secondInt = 214_000_000;
+
+    int result = firstInt + secondInt;
+
+        System.out.println(result);
+
+        boolean big = 6 > 3;
+        System.out.println(big);
+        boolean small = 3 > 9;
+        System.out.println(small);
+        boolean same = 6==6;
+        System.out.println(same);
+        boolean notsame = 6==7;
+        System.out.println(notsame);
+        boolean another = 6<=7;
+        System.out.println(another);
+        boolean last = 6>=7;
+        System.out.println(last);
     }
 }
